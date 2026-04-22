26d58bb9f0b0b88b19551cb934e151e0c9768b8d
lucasgabriel577 <lucasgabrielferraz183@outlook.com>
2026-04-22 11:04:56 -0300

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,5 @@
+nome = input("Digite seu nome: ")
+idade = int(input("digite sua idade: "))
+curso = input("qual e seu curso: ")
+
+print(f"seu nome é {nome}, sua idade é {idade} é você cursa {curso}.")
