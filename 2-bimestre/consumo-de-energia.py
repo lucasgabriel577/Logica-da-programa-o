9e8f06ab3e18c417ab2d33bf351ac47370db9c6e
lucasgabriel577 <lucasgabrielferraz183@outlook.com>
2026-08-07 10:58:08 -0300

    Add files via upload

---
+++ 2-bimestre/consumo-de-energia.py
@@ -0,0 +1,11 @@
+total_consumo = 0.0
+alertas_alto_consumo = 0
+
+for dia in range(1, 8):
+    consumo = float(input(f"Dia {dia}: informe o consumo em kWh: "))
+    total_consumo += consumo
+    if consumo > 20:
+        alertas_alto_consumo += 1
+
+print(f"Consumo total da semana: {total_consumo:.2f} kWh")
+print(f"Dias com consumo acima de 20 kWh: {alertas_alto_consumo}")
